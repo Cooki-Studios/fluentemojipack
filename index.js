3ed8de1a8e2fd7cfc291b514dbafd98673517f53
CookiCode <59518753+Cooki-Studios@users.noreply.github.com>
2022-09-13 12:22:28 +1200

    Updated

--- index.js
+++
@@ -1,8 +0,0 @@
-const output = document.querySelector('.output');
-const fileInput = document.querySelector("#myfiles");
-
-fileInput.addEventListener("change", () => {
-  for (const file of fileInput.files) {
-    output.innerText += `\n${file.name}`;
-  }
-});
